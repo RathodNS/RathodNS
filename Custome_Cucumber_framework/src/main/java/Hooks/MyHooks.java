3eea37872635c830d46f10c04a59443e99ab6c62
Lebron-commits <nandusinghrathod786@gmail.com>
2023-12-24 01:23:25 +0530

    updatd xml for maven integration

--- Custome_Cucumber_framework/src/main/java/Hooks/MyHooks.java
+++ Custome_Cucumber_framework/src/main/java/Hooks/MyHooks.java
@@ -4,9 +4,14 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 
 import context.TestContext;
 import drivers.DriverFactory;
@@ -19,18 +24,38 @@ public class MyHooks {
 
 	private WebDriver driver;
 	private final TestContext context;
+//	public static ExtentTest extenttest;
 	public MyHooks(TestContext context) {
 		this.context=context;
 	}
 	
+	
+	 /**
+     * Before is a cucumber hook that executes before each scenario executes
+     * Initialization of driver is executed
+     *
+     * @param scenario - The Scenario object
+     */
 	@Before
 	public void SetupDrivers(Scenario sceanrio) throws Exception {
 		driver= DriverFactory.getDriver(ConfiLoader.getInstance().GetBrowser());
 		context.driver=driver;
+//		Capabilities caps = ((RemoteWebDriver) driver).getCapabilities();
+//		String browserName = caps.getBrowserName();
+//		String browserVersion = caps.getBrowserVersion();
+		
+//		extenttest.log(Status.INFO, "Running in:"+browserName+" "+browserVersion);
 		System.out.println("Setting up driver for the Sceanrio:"+sceanrio.getName());
 	}
 	
 	
+	
+	/**
+     * After is a cucumber hook that executes after each scenario executes
+     * Screenshot is captured for each failure after each scenario
+     *
+     * @param scenario - The Scenario object
+     */
 	@After
 	public void after(Scenario scenario) throws IOException {
 		if(scenario.isFailed()) {
--- Custome_Cucumber_framework/src/test/java/Runner/TestRunner.java
+++ Custome_Cucumber_framework/src/test/java/Runner/TestRunner.java
@@ -5,10 +5,15 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-
+/**
+ * This Class starts the execution using given cucumber options
+ * @author Rathod
+ *Here Runner class extending AbstractTestNGCucumberTests class to integrate our cucumber Tests with TestNG
+ * And overriding Scenarios method of this class to pass features and Scenarios as data providers
+ */
 @CucumberOptions(
 		features = "src/test/java/features", // Path to feature files
-	    glue = {"steps","Hooks"},// Package where your step definitions are located
+	    glue = {"steps","Hooks"},// Package where your step definitions and hooks are located
 	    monochrome=true,
 	    plugin = {"pretty", "html:target/cucumber-reports","summary",
 	    		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"} // Plugins for test reporting
